9702b12e1502f3fa9d06441574e32654d7b94f00
Kean Tan <keanification@gmail.com>
2021-07-07 09:14:18 -0700

    Making ember-try use yarn and remove embroiderSafe test suite

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
+const { embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.20',
@@ -56,35 +57,7 @@ module.exports = async function () {
           devDependencies: {},
         },
       },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
-      embroiderSafe(),
+      // embroiderSafe(),
       embroiderOptimized(),
     ],
   };
